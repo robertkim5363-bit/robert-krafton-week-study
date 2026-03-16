b93debeca77b5f70456034af2707c4c8e987c5f2
Gyutae Kim <robertkim5363@gmail.com>
2026-03-17 01:00:31 +0900

    Implement find_password function
    Add function to find password based on reversed words.

--- SW-AI-W02-05-TEMPLATE/week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ SW-AI-W02-05-TEMPLATE/week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,8 @@
-# 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
-# 문제 링크: https://www.acmicpc.net/problem/9933
+def find_password(words):
+  word_set = set(words)
+  for word in words:
+    reversed_word = word[::-1]
+    if reversed_word in word_set:
+      length = len(word)
+      mid_index = length // 2
+      return length, word[mid_index]
